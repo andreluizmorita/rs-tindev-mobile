425189fbea7447a2cebd01bf7db49e58688feb5b
André Luiz Morita <amorita.ext@vr.com.br>
2019-08-11 22:30:57 -0300

    Finish mobile tinDev

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
+import io from 'socket.io-client';
 
 import { SafeAreaView, Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
@@ -8,10 +9,13 @@ import api from '../services/api';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ navigation }) {
   const id = navigation.getParam('user');
+
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers () {
@@ -25,6 +29,16 @@ export default function Main({ navigation }) {
     }
     loadUsers();
   }, [id])
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: id }
+    })
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    }, [id]);
+  }, [id]);
 
   async function handleLike(id) {
     const [user, ...rest] = users;
@@ -88,6 +102,18 @@ export default function Main({ navigation }) {
         <Image source={like}/>
       </TouchableOpacity>
     </View>
+    
+    { matchDev && (
+      <View style={styles.matchContainer}>
+        <Image style={styles.matchImage} source={itsamatch} />
+        <Image style={styles.matchAvatar} source={{ uri: matchDev.avatar }} />
+        <Text style={styles.matchName}>{matchDev.name}</Text>
+        <Text>{matchDev.bio}</Text>
+        <TouchableOpacity type="button" onClick={() => setMatchDev(null)}>
+          <Text>FECHAR</Text>
+        </TouchableOpacity>
+      </View>
+    )}
 
   </SafeAreaView>
 }
@@ -103,7 +129,6 @@ const styles = StyleSheet.create({
   logo: {
     marginTop: 30,
   },
-
     
   empty: {
     fontSize: 24,
@@ -179,5 +204,47 @@ const styles = StyleSheet.create({
     },
   },
 
+  matchContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
+  matchImage: {
+    height: 60,
+    resizeMode: 'contain',
+  },  
+
+  matchAvatar: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    borderWidth: 5,
+    borderColor: '#FFF',
+    marginVertical: 30,
+  },
+
+  matchName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+
+  matchBio: {
+    marginTop: 10,
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    lineHeight: 24,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+
+  closeMatch: {
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+    marginTop: 30,
+    fontWeight: 'bold',
+  }
 })
